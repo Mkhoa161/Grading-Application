8774a96f63b4d9891065a3d3836cc6e001af75e7
Mkhoa161 <khoa041116@gmail.com>
2024-10-19 18:08:30 -0600

    Handle post request for submission page

--- grades/models.py
+++ grades/models.py
@@ -7,17 +7,12 @@ class Assignment(models.Model):
     description = models.TextField(blank=True, null=True)
     deadline = models.DateTimeField(blank=True, null=True)
     weight = models.IntegerField(blank=False, null=False)
-    points = models.IntegerField(blank=True, null=True)
-
-    # def __eq__(self, other):
-    #     if not isinstance(other, Assignment):
-    #         return NotImplemented
-    #     return self.id == other.id
+    points = models.IntegerField(blank=False, null=False)
 
 
 class Submission(models.Model):
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    grader = models.ForeignKey(User, on_delete=models.RESTRICT, null=True, related_name='graded_set')
+    grader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='graded_set')
     file = models.FileField(null=False)
     score = models.FloatField(null=True, blank=True)
